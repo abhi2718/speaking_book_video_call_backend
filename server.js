78326cd3809e8729eb6b02d0db331b201a57b522
Abhishek Singh <abhiwebdev2718@gmail.com>
2022-02-15 21:42:46 +0530

    add

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express'),
       port = process.env.PORT ||7070,
       path = require("path"),
       app = express();
+      app.get('/', (req, res)=>{
+        res.json({success:true,message:'API Started'})
+      })
       app.use(express.static(path.resolve(__dirname, './build')))
       app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, './build', 'index.html'));
       });
       app.listen(port,()=> console.log(`listening on port ${port}`));
+      // http://192.168.43.101:7070/connect/#456789?user
